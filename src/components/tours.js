5163e31a99a1c907ccf1c60094574cfd1ed4e538
Laboratorio5Pc4 <Laboratorio5Pc4@UCATEC.local>
2018-09-03 07:26:52 -0400

    fix

--- src/components/tours.js
+++ src/components/tours.js
@@ -13,12 +13,12 @@ class Tours extends Component{
 
                 {/* MAIN BODY */}
                 <div className="mainbody">                
-                    <Grid className="Summary" style={{width: '80%', margin: 'auto'}} >
+                    <Grid className="Summary" style={{width: '100%', margin: 'auto'}} >
                     {this.props.tours123456.map(posts =>
-                    <Link to="/" className="linkTours"> 
-                        <Card shadow={0} style={{width: '400px', height: '400px', margin: '5px'}} key={posts.id}>
+                    <Link to="/" className="linkTours" key={posts.id}> 
+                        <Card shadow={0} style={{width: '400px', height: '400px', margin: '5px'}} >
                             <CardTitle expand style={{color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #F7A509'}}>{posts.title}</CardTitle>
-                            <CardText><i class="far fa-calendar-alt"></i> DURATION: 12 DAYS<br/><br/><i class="fas fa-hotel"></i> HOTEL - <i class="fas fa-shuttle-van"></i> TRANSPORT - <i class="fas fa-utensils"></i> FOOD & DRINKS</CardText>
+                            <CardText><i className="far fa-calendar-alt"></i> DURATION: 12 DAYS<br/><br/><i className="fas fa-hotel"></i> HOTEL - <i className="fas fa-shuttle-van"></i> TRANSPORT - <i className="fas fa-utensils"></i> FOOD & DRINKS</CardText>
                             <CardActions border>
                                 <Button colored>View More</Button>
                             </CardActions>
